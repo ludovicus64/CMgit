cc8aef9367a39a09c5850d2da593a28261d67f24
louis <contact@louisauthie.fr>
2014-01-04 18:22:18 +0100

    Initial1

--- configurateurmousse/translations/fr.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-global $_MODULE;
-$_MODULE = array();
-$_MODULE['<{configurateurmousse}prestashop>configurateurmousse_0fffe5d5d68d8a43e8543fcf7df73dfc'] = 'Découpe mousse sur mesure, c’est le site idéal pour faire ou refaire le matelas de grand-mère avec le confort d’aujourd’hui, remettre à neuf les assises de la banquette du camping-car ou du mobile-home, ou tout simplement, imaginer son propre confort pour faire de son chez-soi, un lieu unique au monde !';
